0ba50ea3c8c0a2438fcce81c8cf03d543cb6ae63
Riccardo <ilficotrani@gmail.com>
2025-11-18 22:40:25 +0100

    Fix: Remove top-level await from database.js, use simple re-export

--- src/config/database.js
+++ src/config/database.js
@@ -2,19 +2,8 @@
 // This file re-exports from server/database.js for Node.js server use
 // When running in Node.js, this file is used instead of database.ts
 
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Calculate absolute path to server/database.js from this file's location
-// src/config/database.js -> ../../server/database.js
-const serverDbPath = join(__dirname, '../../server/database.js');
-
-// Use dynamic import to handle the path correctly
-const serverDb = await import(serverDbPath);
-
-export const { query, getClient, pool } = serverDb;
-export default serverDb.default;
+// Direct re-export using relative path
+// This should work because both files are in the repository root
+export { query, getClient, pool } from '../../server/database.js';
+export { default } from '../../server/database.js';
 
